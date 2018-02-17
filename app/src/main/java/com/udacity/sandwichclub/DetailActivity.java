e8bdf2171b1ffddb18e1ff6a531e41dfb91a9a7e
Lubomir Vaskov <lubovaskov@gmail.com>
2018-02-17 19:42:12 +0200

    JSON data parsed to a Sandwich object Parsed data loaded into detail activity Detail activity layout fixed

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -4,11 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -43,7 +46,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +59,34 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
 
+        TextView placeOfOriginTv = findViewById(R.id.origin_tv);
+        TextView descriptionTv = findViewById(R.id.description_tv);
+        TextView alsoKnownAsTv = findViewById(R.id.also_known_tv);
+        TextView ingredientsTv = findViewById(R.id.ingredients_tv);
+
+        placeOfOriginTv.setText(sandwich.getPlaceOfOrigin());
+        descriptionTv.setText(sandwich.getDescription());
+
+        List<String> alsoKnownAs = sandwich.getAlsoKnownAs();
+        StringBuilder alsoKnownAsText = new StringBuilder();
+        for (String s: alsoKnownAs) {
+            if (alsoKnownAsText.length() > 0) {
+                alsoKnownAsText.append(", ");
+            }
+            alsoKnownAsText.append(s);
+        }
+        alsoKnownAsTv.setText(alsoKnownAsText);
+
+        List<String> ingredients = sandwich.getIngredients();
+        StringBuilder ingredientsText = new StringBuilder();
+        for (String s: ingredients) {
+            if (ingredientsText.length() > 0) {
+                ingredientsText.append(", ");
+            }
+            ingredientsText.append(s);
+        }
+        ingredientsTv.setText(ingredientsText);
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,47 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+
+        Sandwich result;
+
+        try {
+            JSONObject sandwichJSON = new JSONObject(json);
+            JSONObject sandwichNameJSON = sandwichJSON.getJSONObject("name");
+
+            String mainName = sandwichNameJSON.getString("mainName");
+
+            JSONArray alsoKnownAsArray = sandwichNameJSON.getJSONArray("alsoKnownAs");
+            ArrayList<String> alsoKnownAs = new ArrayList<>();
+            for (int i = 0; i < alsoKnownAsArray.length(); i++) {
+                alsoKnownAs.add(alsoKnownAsArray.getString(i));
+            }
+
+            String placeOfOrigin = sandwichJSON.getString("placeOfOrigin");
+            String description = sandwichJSON.getString("description");
+            String image = sandwichJSON.getString("image");
+
+            JSONArray ingredientsArray = sandwichJSON.getJSONArray("ingredients");
+            ArrayList<String> ingredients = new ArrayList<>();
+            for (int i = 0; i < ingredientsArray.length(); i++) {
+                ingredients.add(ingredientsArray.getString(i));
+            }
+
+            result = new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return result;
     }
 }
